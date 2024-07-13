137a769ec0d38716337b4561ad6ef3921df5cb66
Raymond314 <raymond.thomas.314@gmail.com>
2024-07-13 12:24:57 +0200

    In listView for each functionality added

--- ToDoList2/Views/ListView.swift
+++ ToDoList2/Views/ListView.swift
@@ -8,10 +8,20 @@
 import SwiftUI
 
 struct ListView: View {
+    
+    @State var items:[ItemModel] = [
+    ItemModel(title: "First line item", isCompleted: true),
+    ItemModel(title: "Second line item", isCompleted: false),
+    ItemModel(title: "Third line item", isCompleted: true)
+    ]
+    
     var body: some View {
         List {
-           // RowView()
+            ForEach (items) {
+                item in RowView(item: item)
+            }
         }
+        .listStyle(.plain)
         .navigationTitle("To Do List ✅")
         .navigationBarItems(leading: EditButton(),
                             trailing: NavigationLink("Add", destination: AddView()))
