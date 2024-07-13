a4fdf7593c215572273c865e68451e3f12342c37
Raymond314 <raymond.thomas.314@gmail.com>
2024-07-13 11:38:27 +0200

    Baseline

--- ToDoList2/ToDoList2App.swift
+++ ToDoList2/ToDoList2App.swift
@@ -11,7 +11,9 @@ import SwiftUI
 struct ToDoList2App: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            NavigationView {
+                ListView()
+            }
         }
     }
 }
