b54723a439d421464d8b43303c1ad75aa9b72a35
Raymond314 <raymond.thomas.314@gmail.com>
2024-07-13 11:29:55 +0200

    Initial Commit

---
+++ ToDoList2/ToDoList2App.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoList2App.swift
+//  ToDoList2
+//
+//  Created by Raymond Thomas on 13/07/2024.
+//
+
+import SwiftUI
+
+@main
+struct ToDoList2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
