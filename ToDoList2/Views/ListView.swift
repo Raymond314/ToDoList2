a2f10961a82e5ef641fa477722d41b4ded2f43dc
Raymond314 <raymond.thomas.314@gmail.com>
2024-07-13 12:33:25 +0200

    ListView change code moved to ListViewModel

--- ToDoList2/Views/ListView.swift
+++ ToDoList2/Views/ListView.swift
@@ -9,32 +9,20 @@ import SwiftUI
 
 struct ListView: View {
     
-    @State var items:[ItemModel] = [
-    ItemModel(title: "First line item", isCompleted: true),
-    ItemModel(title: "Second line item", isCompleted: false),
-    ItemModel(title: "Third line item", isCompleted: true)
-    ]
+    @EnvironmentObject var listViewModel:ListViewModel
     
     var body: some View {
         List {
-            ForEach (items) {
+            ForEach (listViewModel.items) {
                 item in RowView(item: item)
             }
-            .onDelete(perform: deleteItem)
-            .onMove(perform: moveItem)
+            .onDelete(perform: listViewModel.deleteItem)
+            .onMove(perform: listViewModel.moveItem)
         }
         .listStyle(.plain)
         .navigationTitle("To Do List ✅")
         .navigationBarItems(leading: EditButton(),
                             trailing: NavigationLink("Add", destination: AddView()))
-    }
-    
-    func deleteItem(indexSet:IndexSet) {
-        items.remove(atOffsets: indexSet)
-    }
-    
-    func moveItem(from:IndexSet, to:Int) {
-        items.move(fromOffsets: from, toOffset: to)
     }
 }
 
@@ -42,6 +30,7 @@ struct ListView: View {
     NavigationView {
         ListView()
     }
+    .environmentObject(ListViewModel())
 }
 
 
